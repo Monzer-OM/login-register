378ff3301fbd0da516323c32907f88b1f045e085
Monzer-OM <96489338+Monzer-OM@users.noreply.github.com>
2024-06-12 17:04:22 +0300

    deit the database

--- database.php
+++ database.php
@@ -1,12 +1,17 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "login_register";
+$hostName = "sql12.freesqldatabase.com";
+$dbUser = "sql12713532";
+$dbPassword = "rGJnEZ2cXu";
+$dbName = "sql12713532";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong11111;");
 }
-
+/*
+Host: sql12.freesqldatabase.com
+Database name: sql12713532
+Database user: sql12713532
+Database password: rGJnEZ2cXu
+Port number: 3306*/
 ?>
